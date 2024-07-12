7b772e8f27d35f3ec8f17a49fa01d56d98c6fe26
Gana Ebenezer <54691862+Eben001@users.noreply.github.com>
2024-07-12 11:46:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import requests
+from requests.auth import HTTPProxyAuth
+from pybit.unified_trading import HTTP
+from google.colab import userdata
+import os
+
 
 response = requests.get('https://ipinfo.io')
 
@@ -6,3 +11,33 @@ if response.status_code == 200:
   print(response.text)
 else:
   print('Error occurred:', response.status_code)
+
+
+PROXY = os.environ['proxy']
+
+os.environ['HTTP_PROXY'] = PROXY
+os.environ['HTTPS_PROXY'] = PROXY
+
+
+API_KEY = os.environ['api_key']
+API_SECRET = os.environ['api_secret']
+session = HTTP(
+    testnet=True,
+    api_key=API_KEY,
+    api_secret=API_SECRET,
+)
+
+def get_balance():
+    try:
+        resp = session.get_wallet_balance(accountType="UNIFIED", coin="USDT")['result']['list'][0]['coin'][0]['walletBalance']
+        resp = float(resp)
+        return resp
+    except Exception as err:
+        print(err)
+
+print(f'Your balance: {get_balance()} USDT')
+
+
+
+
+
